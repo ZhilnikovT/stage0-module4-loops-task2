cf1088e3fb59c416c15d3ed62498bc34827466c2
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-25 08:20:24 +0300

    Update EvenNumbersPrinter.java

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
+    
+     public static void main(String[] args) {
+        EvenNumbersPrinter g = new EvenNumbersPrinter();
+        g.printEvenNumbers(20);
+    }
     public void printEvenNumbers(int printTillInclusive) {
+        int i = 0;
+        while(i <= printTillInclusive){
+            if (i%2 == 0){System.out.println(i);}
+             i++;
+        }
     }
 }
