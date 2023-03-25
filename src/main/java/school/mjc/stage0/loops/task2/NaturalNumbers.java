c0dcddbded98ba5a9705362541aed6429be2c334
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-25 08:13:41 +0300

    Update NaturalNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
+    
+    public static void main(String[] args) {
+        NaturalNumbers g = new NaturalNumbers();
+        g.naturalNumbersPrinter(20);
+    }
+    
     public void naturalNumbersPrinter(int lastPrinted) {
+        for(int i = 0; i <= lastPrinted; i++){
+            System.out.println(i);
+        }
     }
 }
