89288edb9571321300c46cb0001bcfa1c5b0085c
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-25 15:26:28 +0300

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,7 +1,18 @@
 package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
+    
+    public static void main(String[] args) {
+        WordsBuilder g = new WordsBuilder();
+        g.buildPhrase('q','w');
+    }
+    
     public void buildPhrase(char... chars) {
-
+        String s = "";
+        int i; 
+        for(i = 0; i < chars.length; i++){
+            s = s + chars[i];
+        }
+        System.out.print(s);
     }
 }
